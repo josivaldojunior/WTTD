d01929291834ac0d59ba26a7bbcfc7eedb044f93
Josivaldo Junior <josivaldodacostajunior@gmail.com>
2020-03-01 18:30:23 -0300

    2°commit

--- eventex/settings.py
+++ eventex/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'defautl': config('DATABASE_URL', default_dburl,cast=dburl),
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
+
 
 
 # Password validation
